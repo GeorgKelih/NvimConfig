d7c6084886134d4e2d8083de4ffad058685277ae
GeorgKelih <georg.kelih@gmail.com>
2025-05-29 17:30:48 +0200

    Terminal to normal mode now with esc improved tab

--- lua/plugins/tab-autocomplete.lua
+++ lua/plugins/tab-autocomplete.lua
@@ -1,24 +1,24 @@
 return {
   {
-    "saghen/blink.cmp", -- Or "hrsh7th/nvim-cmp" if you're using that instead
+    "saghen/blink.cmp",
     opts = function(_, opts)
       opts.keymap = {
-        preset = "super-tab",
-        ["<Tab>"] = { "select_and_accept" },
-        ["<S-Tab>"] = { "select_prev" },
+        preset = "super-tab", -- This preset should handle fallback intelligently
+        -- If "super-tab" isn't behaving as expected for fallback,
+        -- you might need to define the Tab mapping explicitly and use its fallback mechanism.
+        -- ["<Tab>"] = function(cmp)
+        --   if cmp.visible() then
+        --     return cmp.select_and_accept() -- or just select_next/accept
+        --   elseif cmp.snippet_active() then
+        --     return cmp.snippet_forward()
+        --   elseif --[[ some condition like has_words_before() adapted for blink.cmp ]] then
+        --     return cmp.complete()
+        --   else
+        --     return cmp.fallback() -- blink.cmp should also have a fallback mechanism
+        --   end
+        -- end,
       }
-      -- If you also want to use Tab to expand/jump through snippets,
-      -- you might need more advanced mapping.
-      -- Here's a more comprehensive example from a Reddit discussion for nvim-cmp:
-      -- if opts.keymap["<Tab>"] then
-      --   if opts.keymap.preset == "super-tab" then
-      --     -- super-tab
-      --     opts.keymap["<Tab>"] = {
-      --       require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-      --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-      --     }
-      --   end
-      -- end
+      return opts
     end,
   },
 }
